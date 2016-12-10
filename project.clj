fbd9d19d08eab0aafb80b351faace90a9198f131
Oskar Kvist <oskar.kvist@gmail.com>
2016-12-10 10:14:13 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,15 +1,19 @@
 (defproject rureader "0.1.0"
-  :plugins [[lein-cljsbuild "1.1.3"]]
-  :dependencies [[org.clojure/clojurescript "1.8.40"]
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :dependencies [[org.clojure/clojurescript "1.9.293"]
                  [org.clojure/clojure "1.8.0"]
-                 [com.cognitect/transit-cljs "0.8.237"]]
-  :cljsbuild {
-    :builds [{
-        ; The path to the top-level ClojureScript source directory:
-        :source-paths ["src-cljs"]
-        ; The standard ClojureScript compiler options:
-        ; (See the ClojureScript compiler documentation for details.)
-        :compiler {
-          :output-to "target/cljsbuild-main.js"
-          :optimizations :whitespace
-          :pretty-print true}}]})
+                 [com.cognitect/transit-cljs "0.8.239"]]
+  :source-paths ["src-cljs"]
+  :cljsbuild {:builds
+              [{
+                ; The path to the top-level ClojureScript source directory:
+                :source-paths ["src-cljs"]
+                ; The standard ClojureScript compiler options:
+                ; (See the ClojureScript compiler documentation for details.)
+                :compiler {:output-to "target/cljsbuild-main.js"
+                           :optimizations :whitespace
+                           :pretty-print true}}]}
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                   :repl-options {:nrepl-middleware
+                                  [cemerick.piggieback/wrap-cljs-repl]}}})
